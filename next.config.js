cbd9bfd69d1ff351a1e7d39ae4853011ea26ac80
Lawlighty_li <Lawlighty_li@southbaytech.com>
2021-07-24 21:29:57 +0800

    BUG :  antd 按需加载挂载为全局

--- next.config.js
+++ next.config.js
@@ -4,4 +4,30 @@ if (typeof require !== "undefined") {
   require.extensions[".css"] = (file) => {};
 }
 
+module.exports = {
+  webpack(config, { isServer }) {
+    if (isServer) {
+      const antStyles = /antd-mobile\/.*?\/style.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === "function" ? [] : origExternals),
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: "null-loader",
+      });
+    }
+    return config;
+  },
+};
+
 module.exports = withCss({});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/global.css";
+import "antd/dist/antd.css"; //全局引用antd的样式
 import { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
